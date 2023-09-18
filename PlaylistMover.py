ae478184359c8a0cb1805a6e09c5486207eff001
GoldenKain <nico.parr@hotmail.com>
2023-09-17 20:22:05 -0400

    Fixed bug where some lines paths would not have a line break afterwards

--- PlaylistMover.py
+++ PlaylistMover.py
@@ -13,25 +13,26 @@ if not playlistFile.lower().endswith('.m3u'):
     print('Error. {playlistFile} is not a valid playlist file (.m3u).')
     exit()
 
-with open(playlistFile, 'r') as file:
-    with open(file.name + '_new', 'w') as newfile:
+with open(playlistFile, 'rt') as file:
+    with open(file.name + '_new', 'wt') as newfile:
         for l in file.readlines():
             if l.startswith('#'):
                 newfile.write(l)
                 continue
 
+            # This function call gets rid of trailing whitespaces. I have to manually add a line break from this point on.
             l = unquote(urlparse(l).path, errors='replace')
 
             if path.exists(l):
-                newfile.write(l)
+                newfile.write(l + '\n')
                 continue
 
-            globStr = path.join(path.expanduser('~'), 'Music', '**', escape(path.splitext(path.basename(l.strip()))[0]))
+            globStr = path.join(path.expanduser('~'), 'Music', '**', escape(path.splitext(path.basename(l))[0]))
 
             results = glob(globStr + '.mp3', recursive=True) + glob(globStr + '.flac', recursive=True)
 
             if len(results) < 1:
-                newfile.write('#' + l)
+                newfile.write('#' + l + '\n')
                 continue
 
             for r in results:

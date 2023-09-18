910244a864284e3b6a50f59a30cb1548e6b64261
GoldenKain <nico.parr@hotmail.com>
2023-09-17 20:59:51 -0400

    Now, multiple playlist files can be passed as arguments to be parsed

--- PlaylistMover.py
+++ PlaylistMover.py
@@ -3,44 +3,47 @@ from os import path, rename
 from glob import glob, escape
 from urllib.parse import unquote, urlparse
 
-playlistFile = sys.argv[1]
-
-if not path.exists(playlistFile):
-    print('Error. {playlistFile} doesn\'t exist.')
+if len(sys.argv) < 2:
+    print('Error. Missing playlist files to process in arguments.')
     exit()
 
-if not playlistFile.lower().endswith('.m3u'):
-    print('Error. {playlistFile} is not a valid playlist file (.m3u).')
-    exit()
+for playlistFile in sys.argv[1:]:
+    if not path.exists(playlistFile):
+        print('Error. {playlistFile} doesn\'t exist.')
+        continue
 
-with open(playlistFile, 'rt') as file:
-    with open(file.name + '_new', 'wt') as newfile:
-        for l in file.readlines():
-            if l.startswith('#'):
-                newfile.write(l)
-                continue
+    if not playlistFile.lower().endswith('.m3u'):
+        print('Error. {playlistFile} is not a valid playlist file (.m3u).')
+        continue
 
-            # This function call gets rid of trailing whitespaces. I have to manually add a line break from this point on.
-            l = unquote(urlparse(l).path, errors='replace')
+    with open(playlistFile, 'rt') as file:
+        with open(file.name + '_new', 'wt') as newfile:
+            for l in file.readlines():
+                if l.startswith('#'):
+                    newfile.write(l)
+                    continue
 
-            if path.exists(l):
-                newfile.write(l + '\n')
-                continue
+                # This function call gets rid of trailing whitespaces. I have to manually add a line break from this point on.
+                l = unquote(urlparse(l).path, errors='replace')
 
-            globStr = path.join(path.expanduser('~'), 'Music', '**', escape(path.splitext(path.basename(l))[0]))
+                if path.exists(l):
+                    newfile.write(l + '\n')
+                    continue
 
-            results = glob(globStr + '.mp3', recursive=True) + glob(globStr + '.flac', recursive=True)
+                globStr = path.join(path.expanduser('~'), 'Music', '**', escape(path.splitext(path.basename(l))[0]))
 
-            if len(results) < 1:
-                newfile.write('#' + l + '\n')
-                continue
+                results = glob(globStr + '.mp3', recursive=True) + glob(globStr + '.flac', recursive=True)
 
-            for r in results:
-                if r.endswith('.flac'):
-                    newfile.write(r + '\n')
-                    break
-            else:
-                newfile.write(results[0] + '\n')
+                if len(results) < 1:
+                    newfile.write('#' + l + '\n')
+                    continue
 
-rename(playlistFile, playlistFile + '_old')
-rename(playlistFile + '_new', playlistFile)
+                for r in results:
+                    if r.endswith('.flac'):
+                        newfile.write(r + '\n')
+                        break
+                else:
+                    newfile.write(results[0] + '\n')
+
+    rename(playlistFile, playlistFile + '_old')
+    rename(playlistFile + '_new', playlistFile)
